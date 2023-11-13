61ae278e01f0a110b66ef6e99b4bc26f68726acd
dnovik01 <dnovik01@gmail.com>
2023-11-13 10:14:39 +0300

    added function sortStrings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,14 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
+export function sortStrings(arr, param = 'asc') {
+    let copyArr = arr.slice()
+    let rez = copyArr.sort(function (a, b) {
+        return a.localeCompare(b, undefined, { sensitivity: 'case', caseFirst: 'upper' });
+    })
+    if (param == 'desc') {
+        return rez.reverse();
+    }
+    return rez
 }
