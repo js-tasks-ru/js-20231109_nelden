ab441451e3c59667537612272318d7743fdb8710
Novik Denis Seregeevich <dnovik@ozon.ru>
2023-11-18 12:52:31 +0300

    added solution for create getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,8 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const fields = path.split('.');
+  return function (obj) {
+    return fields.reduce((current, field) => current && current[field], obj);
+  };
 }
