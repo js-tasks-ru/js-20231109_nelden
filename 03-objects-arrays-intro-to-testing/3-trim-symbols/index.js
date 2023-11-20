6a288f99b83d6f0adcade37cfa0ff4cf8815af4e
dnovik <dnovik@ozon.ru>
2023-11-20 09:35:09 +0300

    added solution for 03 trim symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,38 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
 
+
+function getTrimmedStr(string, size) {
+  let result = '';
+  let lastChar = '';
+  let count = 0;
+ 
+  for (let char of string) {
+ 
+    if (char === lastChar) {
+      count++;
+    } else {
+      count = 1;
+    }
+ 
+    if (count <= size) {
+      result += char;
+    }
+ 
+    lastChar = char;
+  }
+ 
+  return result;
+}
+ 
+
+export function trimSymbols(string, size) {
+  if (size === undefined) {
+    return string;
+  } else if (size == 0) {
+    return '';
+  } else {
+    return getTrimmedStr(string, size);
+  }
 }
