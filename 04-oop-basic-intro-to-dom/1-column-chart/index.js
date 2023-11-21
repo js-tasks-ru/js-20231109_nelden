4057f44f9120d4dc5efba3ce3c2f55f4e5e93e00
dnovik <dnovik@ozon.ru>
2023-11-21 13:55:08 +0300

    added createElement method

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -4,37 +4,18 @@ export default class ColumnChart {
     this.label = label;
     this.link = link;
     this.value = value;
+    this.element = this.createElement();
   }
 
-  getChartOrders() {
-    return `<div class="dashboard__chart_orders">
-    <div class="column-chart" style="--chart-height: 50">
-      <div class="column-chart__title">
-        Total orders
-        <a href="/sales" class="column-chart__link">View all</a>
-      </div>
-      <div class="column-chart__container">
-        <div data-element="header" class="column-chart__header">344</div>
-        <div data-element="body" class="column-chart__chart">
-          <div style="--value: 2" data-tooltip="6%"></div>
-          <div style="--value: 22" data-tooltip="44%"></div>
-          <div style="--value: 5" data-tooltip="11%"></div>
-          <div style="--value: 50" data-tooltip="100%"></div>
-          <div style="--value: 12" data-tooltip="25%"></div>
-          <div style="--value: 4" data-tooltip="8%"></div>
-          <div style="--value: 13" data-tooltip="28%"></div>
-          <div style="--value: 5" data-tooltip="11%"></div>
-          <div style="--value: 23" data-tooltip="47%"></div>
-          <div style="--value: 12" data-tooltip="25%"></div>
-          <div style="--value: 34" data-tooltip="69%"></div>
-          <div style="--value: 1" data-tooltip="3%"></div>
-          <div style="--value: 23" data-tooltip="47%"></div>
-          <div style="--value: 27" data-tooltip="56%"></div>
-          <div style="--value: 2" data-tooltip="6%"></div>
-          <div style="--value: 1" data-tooltip="3%"></div>
-        </div>
-      </div>
-    </div>
-  </div>`;
+  createElement() {
+    let element = document.createElement('div');
+    element.dataset.element = "body";
+    element.className = "column-chart__chart";
+    this.data.forEach(element => {
+      let divValue = document.createElement('div');
+      divValue.style.setProperty('--value', element);
+      divValue.dataset.tooltip = "6%";
+      element.appendChild(divValue);
+    });
   }
 }
