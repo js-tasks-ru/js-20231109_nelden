8eee325183b3263532930be377d573bf360a8436
dnovik01 <dnovik01@gmail.com>
2023-11-22 22:59:00 +0300

    update

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,10 +1,31 @@
 export default class ColumnChart {
-  constructor(data = [], label = "", link = "", value = 0) {
-    this.data = data;
-    this.label = label;
-    this.link = link;
-    this.value = value;
+  constructor(props) {
+    this.data = !Array.isArray(props.data) ? 0 : props.data;
+    this.total = !Array.isArray(props.data)
+      ? 0
+      : props.data.reduce((acc, item) => acc + item, 0);
+    this.label = props.label == "" ? "" : props.label;
+    this.link = props.link;
+    this.value = props.value;
     this.element = this.createElement(this.createTemplate());
+  }
+
+  createTemplate() {
+    return `
+    <div id="${this.label}" class="dashboard__chart_${this.label}">
+    <div class="column-chart" style="--chart-height: 50">
+      <div class="column-chart__title">
+        Total ${this.label}
+        <a href="/sales" class="column-chart__${this.link}">View all</a>
+      </div>
+      <div class="column-chart__container">
+        <div data-element="header" class="column-chart__header">
+        ${this.value}</div>
+        <div data-element="body" class="column-chart__chart">
+        ${this.createChartBodyTemplate()}
+        </div>
+      </div>
+    </div>`;
   }
 
   createElement(template) {
@@ -14,28 +35,15 @@ export default class ColumnChart {
   }
 
   createChartBodyTemplate() {
-    if (!Array.isArray(this.data)) {
+    if (this.data == 0) {
       return `<img src='./charts-skeleton.svg'></img>`;
     }
     return this.data
       .map(
-        (element) => `<div style="--value: ${element}" data-tooltip="6%"></div>`
+        (element) =>
+          `<div style="--value: ${element}" 
+          data-tooltip=${(element / this.total) * 100}%></div>`
       )
       .join("");
-  }
-
-  createTemplate() {
-    return `<div class="column-chart" style="--chart-height: 50">
-      <div class="column-chart__title">
-        Total ${this.label}
-        <a href="/sales" class="column-chart__link">View all</a>
-      </div>
-      <div class="column-chart__container">
-        <div data-element="header" class="column-chart__header">344</div>
-        <div data-element="body" class="column-chart__chart">
-        ${this.createChartBodyTemplate()}
-        </div>
-      </div>
-    </div>`;
   }
 }
