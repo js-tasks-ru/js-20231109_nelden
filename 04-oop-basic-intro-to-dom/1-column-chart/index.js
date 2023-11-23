9a6e3ae7ed434b602905d36b3f071561bc87d7a5
dnovik01 <dnovik01@gmail.com>
2023-11-23 10:07:07 +0300

    fix

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,10 +1,10 @@
 export default class ColumnChart {
   constructor(props) {
-    this.data = !Array.isArray(props.data) ? 0 : props.data;
+    this.data = !Array.isArray(props.data) ? [0] : props.data;
     this.total = !Array.isArray(props.data)
       ? 0
       : props.data.reduce((acc, item) => acc + item, 0);
-    this.label = props.label == "" ? "" : props.label;
+    this.label = props.label == "" ? "Empty label" : props.label;
     this.link = props.link;
     this.value =
       this.label == "sales"
@@ -13,42 +13,55 @@ export default class ColumnChart {
     this.element = this.createElement(this.createTemplate());
   }
 
-  createTemplate() {
-    if (this.data) {
-      return `
-    <div id="${this.label}" class="dashboard__chart_${this.label}">
-    <div class="column-chart" style="--chart-height: 50">
-      <div class="column-chart__title">
-        Total ${this.label}
-        <a href="/sales" class="column-chart__${this.link}">View all</a>
-      </div>
-      <div class="column-chart__container">
-        <div data-element="header" class="column-chart__header">
-        ${this.value}</div>
-        <div data-element="body" class="column-chart__chart">
-        ${this.createChartBodyTemplate()}
-        </div>
-      </div>
-    </div>`;
-    }
-  }
-
   createElement(template) {
     const element = document.createElement("div");
     element.innerHTML = template;
     return element.firstElementChild;
   }
 
-  createChartBodyTemplate() {
-    if (this.data == 0) {
+  createTemplate() {
+    if (this.data) {
+      return `
+    <div id="${this.label}" class="dashboard__chart_${this.label}">
+    <div class="column-chart" style="--chart-height: 50">
+      <div class="column-chart__title">${this.label}
+        <a href="/${this.label}" class="column-chart__link">${this.link}</a>
+      </div>
+      <div class="column-chart__container">
+        <div data-element="header" class="column-chart__header">
+        ${this.value}</div>
+        <div data-element="body" class="column-chart__chart">
+        ${this.createChartBodyTemplate(this.data)}
+        </div>
+      </div>
+    </div>`;
+    }
+  }
+
+  createChartBodyTemplate(data) {
+    const maxValue = Math.max(...this.data);
+    const scale = 50 / maxValue;
+    if (data == 0) {
       return `<img src='./charts-skeleton.svg'></img>`;
     }
     return this.data
       .map(
-        (element) =>
-          `<div style="--value: ${(element / this.total) * 100}" 
-          data-tooltip=${(element / this.total) * 100}%></div>`
+        (item) =>
+          `<div style="--value: ${String(Math.floor(item * scale))}" 
+          data-tooltip=${((item / maxValue) * 100).toFixed(0) + "%"}></div>`
       )
       .join("");
+  }
+
+  update(newData) {
+    this.data = !Array.isArray(newData) ? 0 : newData;
+    this.total = !Array.isArray(newData)
+      ? 0
+      : newData.reduce((acc, item) => acc + item, 0);
+
+    const newChartBodyTemplate = this.createChartBodyTemplate(this.data);
+
+    this.element.querySelector(".column-chart__chart").innerHTML =
+      newChartBodyTemplate;
   }
 }
