110e253edfa633fe79b5e13e09368e09d431878f
dnovik01 <dnovik01@gmail.com>
2023-12-01 20:54:45 +0300

    sync

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -14,28 +14,18 @@ export default class SortableTable {
     return div;
   }
 
-  createTableHeaderTemplate() {
-    return `
-    <div data-element="header" class="sortable-table__header sortable-table__row">
-      <div class="sortable-table__cell" data-id="images" data-sortable="false" data-order="asc">
-        <span>Image</span>
-      </div>
-      <div class="sortable-table__cell" data-id="title" data-sortable="true" data-order="asc">
-        <span>Name</span>
-        <span data-element="arrow" class="sortable-table__sort-arrow">
-          <span class="sort-arrow"></span>
-        </span>
-      </div>
-      <div class="sortable-table__cell" data-id="quantity" data-sortable="true" data-order="asc">
-        <span>Quantity</span>
-      </div>
-      <div class="sortable-table__cell" data-id="price" data-sortable="true" data-order="asc">
-        <span>Price</span>
-      </div>
-      <div class="sortable-table__cell" data-id="sales" data-sortable="true" data-order="asc">
-        <span>Sales</span>
-      </div>
-  </div>`;
+  createTableHeaderTemplate(sortOrder = "asc") {
+    const div = document.createElement("div");
+    div.innerHTML =
+      '<div data-element="header" class="sortable-table__header sortable-table__row"></div>';
+    div.outerHTML = this.headerConfig
+      .map(
+        (item) => `
+      <div class="sortable-table__cell" data-id=${item.id} data-sortable=${item.sortable} data-order=${sortOrder}>
+        <span>${item.title}</span>`
+      )
+      .join();
+    return div;
   }
 
   createTableBodyTemplate() {
