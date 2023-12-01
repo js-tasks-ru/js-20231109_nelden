de9681e417963db6da74070b6bb78ae6bce3f12c
dnovik01 <dnovik01@gmail.com>
2023-12-01 09:21:59 +0300

    initial commit

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -15,25 +15,26 @@ export default class SortableTable {
   }
 
   createTableHeaderTemplate() {
-    return `<div data-element="header" class="sortable-table__header sortable-table__row">
-    <div class="sortable-table__cell" data-id="images" data-sortable="false" data-order="asc">
-      <span>Image</span>
-    </div>
-    <div class="sortable-table__cell" data-id="title" data-sortable="true" data-order="asc">
-      <span>Name</span>
-      <span data-element="arrow" class="sortable-table__sort-arrow">
-        <span class="sort-arrow"></span>
-      </span>
-    </div>
-    <div class="sortable-table__cell" data-id="quantity" data-sortable="true" data-order="asc">
-      <span>Quantity</span>
-    </div>
-    <div class="sortable-table__cell" data-id="price" data-sortable="true" data-order="asc">
-      <span>Price</span>
-    </div>
-    <div class="sortable-table__cell" data-id="sales" data-sortable="true" data-order="asc">
-      <span>Sales</span>
-    </div>
+    return `
+    <div data-element="header" class="sortable-table__header sortable-table__row">
+      <div class="sortable-table__cell" data-id="images" data-sortable="false" data-order="asc">
+        <span>Image</span>
+      </div>
+      <div class="sortable-table__cell" data-id="title" data-sortable="true" data-order="asc">
+        <span>Name</span>
+        <span data-element="arrow" class="sortable-table__sort-arrow">
+          <span class="sort-arrow"></span>
+        </span>
+      </div>
+      <div class="sortable-table__cell" data-id="quantity" data-sortable="true" data-order="asc">
+        <span>Quantity</span>
+      </div>
+      <div class="sortable-table__cell" data-id="price" data-sortable="true" data-order="asc">
+        <span>Price</span>
+      </div>
+      <div class="sortable-table__cell" data-id="sales" data-sortable="true" data-order="asc">
+        <span>Sales</span>
+      </div>
   </div>`;
   }
 
@@ -57,5 +58,15 @@ export default class SortableTable {
             <div class="sortable-table__cell">${item.sales}</div>
           </a>`;
     });
+  }
+
+  sort(fieldValue, orderValue) {}
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
   }
 }
